c1ee86b90ee394b0bf56908e41cccb6e54f4f64f
meet-kp <119094821+meet-kp@users.noreply.github.com>
2022-12-19 18:01:41 +0530

    Add files via upload

---
+++ button state task1.ino
@@ -0,0 +1,30 @@
+
+#define button 2 //oush button at pin 2
+ const int ledPin[] = {4, 5, 6, 7, 8, 9};  // creating a array for  6 leds
+ for (index = 4; index <= 9; index++);
+ {
+    pinMode(ledPin[index], OUTPUT);
+ }
+   pinMode(button,INPUT);
+   int count=0;
+
+
+    void setup(){connect to wifi code here }
+    void loop()
+{ 
+ count = (count=0; count <= 6; count++);   
+  
+  if count=1;
+  digitalWrite(ledPin 4, HIGH);
+  elif count=2;
+  digitalWrite(ledPin 5, HIGH);
+  elif count=3;
+  digitalWrite(ledPin 6, HIGH);  
+  elif count=4;
+  digitalWrite(ledPin 7, HIGH);
+  elif count=5;
+  digitalWrite(ledPin 8, HIGH);
+  else count=6;
+  digitalWrite(ledPin 9, HIGH);
+  
+}
